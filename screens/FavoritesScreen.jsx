d9b102640fed8a1df64d7d316e0f05e4b6ba5ecd
Marina <marina.p@carbonclick.com>
2023-09-19 00:49:36 +0200

    restored data

---
+++ screens/FavoritesScreen.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+
+const FavoritesScreen = () => {
+  return <Text>The favorites screen!</Text>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+  },
+});
+
+export default FavoritesScreen;
