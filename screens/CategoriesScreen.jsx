a8fc9aa369b4a553ae45f2f464afc4bc61b4399e
Marina <marina.p@carbonclick.com>
2023-09-19 00:30:02 +0200

    added stack navigation

---
+++ screens/CategoriesScreen.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { View, StyleSheet, FlatList } from 'react-native';
+
+import CategoryGridTile from '../components/CategoryGridTile';
+import { CATEGORIES } from '../data/dummy-data';
+
+const CategoriesScreen = ({ navigation }) => {
+
+  const renderCategoryItem = (itemData) => {
+    const pressHandler = () => {
+      navigation.navigate(`MealsOverview`, {
+        categoryId: itemData.item.id,
+      });
+    }
+  
+    return (
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPress={pressHandler}
+      />
+    );
+  };
+
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+      numColumns={2}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {},
+});
+
+export default CategoriesScreen;
